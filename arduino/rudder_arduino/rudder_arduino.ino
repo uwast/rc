501c7f3997b41bfa363d5756ce6bced7c46994bb
Seamus Johnston <seamusbjohnston@gmail.com>
2017-03-22 02:50:02 -0400

    Updated arduino servo code

--- arduino/rudder_arduino/rudder_arduino.ino
+++ arduino/rudder_arduino/rudder_arduino.ino
@@ -1,31 +1,32 @@
+#if (ARDUINO >= 100)
+ #include <Arduino.h>
+#else
+ #include <WProgram.h>
+#endif
+
+#include <Servo.h> 
 #include <ros.h>
-#include <geometry_msgs/Twist.h>
-#include <Servo.h>
+#include <std_msgs/Float32.h>
 
 ros::NodeHandle  nh;
-Servo rudder;
 
-//Read value from cmd_vel
-//Multiply by 60 and add to middle position (90) and write to motor 
-//Conversion (-1.0, 1.0) -> (30, 150)
-void messageCb( const geometry_msgs::Twist& position_msg){
-  float dataVal = position_msg.angular.z; 
-  rudder.write(90+(dataVal*60)); 
+Servo servo;
+
+void servo_cb( const std_msgs::Float32& cmd_msg){
+  servo.write(cmd_msg.data); 
 }
 
-ros::Subscriber<geometry_msgs::Twist> sub("cmd_vel", &messageCb );
 
-void setup()
-{ 
-  pinMode(3, OUTPUT);
+ros::Subscriber<std_msgs::Float32> sub("rudder", servo_cb);
+
+void setup(){
   nh.initNode();
   nh.subscribe(sub);
-  rudder.attach(3);
+  
+  servo.attach(3); //attach it to pin 3
 }
 
-void loop()
-{  
+void loop(){
   nh.spinOnce();
-  delay(10); //Check for message every 10 milliseconds
+  delay(10);
 }
-
