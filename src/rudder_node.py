524c4043f462cc0e5ba9b2f0646c41ac1fe16dcd
Matthew Reynolds <rey.mtr@gmail.com>
2017-11-13 23:59:02 -0500

    Began to implement autonomous rudder control
    Still missing the implementation of the PID, but wrote the callback that will work together with the boat_nav/path_planning_node to set the rudder's position autonomously

--- src/rudder_node.py
+++ src/rudder_node.py
@@ -2,12 +2,65 @@
 import rospy
 from sensor_msgs.msg import Joy
 from std_msgs.msg import Float32
-from std_msgs.msg import Bool      
+from std_msgs.msg import Bool
+from boat_nav.msg import BoatState
 import time
 
+# Declare global variables needed for the node
 tacking = False  # Whether or not we are tacking
 rudder_pos = 90  # What the rudder position is (0-180)
-pub = rospy.Publisher('rudder', Float32, queue_size=10)
+cur_boat_heading = 0
+wind_heading = 0
+state = 0
+
+# Declare the publishers for the node
+rudder_pos_pub = rospy.Publisher('rudder', Float32, queue_size=10)
+boat_state_pub = rospy.Publisher('boat_state', BoatState, queue_size=10)
+
+
+# If the boat state topic changes, update local boat state
+def boat_state_callback(new_state):
+	global state
+	state = new_state
+	
+	
+# If the wind heading topic changes, update local wind heading
+def wind_callback(heading):
+	global wind_heading
+	wind_heading = heading
+	
+	
+# If the boat heading topic changes, update rudder PID setpoint
+def heading_callback(target_heading):
+	global wind_heading
+	global state
+	global cur_boat_heading
+	
+	# Perhaps a worthwhile check, but not really super important because this callback will never be called if these conditions are not met in path_planning_node
+	if state.major is not BoatState.MAJ_AUTONOMOUS or state.minor is not BoatState.MIN_PLANNING:
+		return
+	
+	# We have a new valid setpoint, therefore output it	
+	rospy.loginfo(rospy.get_caller_id() + " New rudder setpoint: %f", target_heading)
+
+	# If the current heading and the new heading are on opposite sides of the wind, we need to tack
+	if (target_heading < wind_heading and cur_boat_heading < wind_heading) or (target_heading > wind_heading and cur_boat_heading > wind_heading):
+		state.minor = BoatState.MIN_TACKING
+		boat_state_pub.publish(state)		
+		rospy.loginfo(rospy.get_caller_id() + " Boat State = 'Autonomous - Tacking'")
+		
+		controller.setTarget(target_heading)
+		while controller.isOnTarget():
+			pass
+			
+		state.minor = BoatState.MIN_PLANNING
+		boat_state_pub.publish(state)		
+		rospy.loginfo(rospy.get_caller_id() + " Boat State = 'Autonomous - Planning'")
+	
+	# Otherwise, we don't need to tack, so simply update the controller's setpoint
+	else:		
+		controller.setSetpoint(target_heading)
+
 
 
 def joy_callback(controller):
@@ -25,7 +78,7 @@ def joy_callback(controller):
 
         # Only publish if the change in rudder angle is greater than 5
         if abs(position_msg.data - rudder_pos_old) > 5:
-            pub.publish(position_msg)
+            rudder_pos_pub.publish(position_msg)
             rospy.loginfo(rospy.get_caller_id() + " Read value: %f", data.axes[0])
             rudder_pos = position_msg.data
 
@@ -41,6 +94,9 @@ def listener():
     rospy.init_node('joy_to_rudder', anonymous=True)
     rospy.Subscriber('joy', Joy, joy_callback)
     rospy.Subscriber('tacking', Bool, tacking_callback)
+    rospy.Subscriber('boat_state', BoatState, boat_state_callback)
+    rospy.Subscriber('wind_heading', Float32, wind_callback)
+    rospy.Subscriber('target_heading', Float32, heading_callback)
     rospy.spin()
     
 
