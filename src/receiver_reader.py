d9397d95182239751673ddf2dad4b008352c5379
Tina Gao <thgao@uwaterloo.ca>
2017-12-23 20:41:07 -0500

    remapped for futaba, still working on publishing joytick + VR message to joy

--- src/receiver_reader.py
+++ src/receiver_reader.py
@@ -24,63 +24,60 @@ counter = 0
 
 class parse_receiver:
     def __init__(self, data):
-        # Each string of information sent from the controller is sent in the following format:
-        # s 4 4 4 4 4 4 4 4 e\r\n
-        # The following code deletes the start and end characters and splits the string of values into an list of values
-        data = data.replace('s ', "")
-        data = data.replace(' e', "")
-        data = data.replace('\r\n', "")
-        self.data_list = data.split(' ')
+        # Sort input data into 8 channels
+        self.channel = []
+        self.channel.append(data[0])
+        self.channel.append((((data[2] & 0x0007) << 8) | data[1]))
+        self.channel.append((((data[2] & 0x00F7) >> 3) | ((data[3] & 0x003F) << 5)))
+        self.channel.append((((data[3] & 0x00C0) >> 6) | (data[4] << 2) | ((data[5] & 0x0001) << 10)))
+        self.channel.append((((data[5] & 0x00FE) >> 1) | ((data[6] & 0x000F) << 7)))
+        self.channel.append((((data[6] & 0x00F0) >> 4) | ((data[7] & 0x007F) << 4)))
+        self.channel.append((((data[7] & 0x0080) >> 7) | (data[8] << 1) | ((data[9] & 0x0003) << 9)))
+        self.channel.append((((data[9] & 0x00FC) >> 2) | ((data[10] & 0x001F) << 6)))
+
+        # each channel has 11 bits
+
+        #self.data_list = data.split(' ')
 
     def parse(self):
         try:
             self.axes = []
             self.buttons = []
-            joystick_temp = self.data_list[:4]
-            dpad_temp = []
-            button_temp = []
+            
+            buttons_temp = []
+            joystick_temp = []
 
-            # Split the list of values into joystick and button controls
-            for i in range(4, 8):
-                dpad_temp.append(self.data_list[i])
+            for i in range(1, 5): #switch A, B, C, D (channels 1 to 4)
+                buttons_temp.append(self.channel[i])
 
-            for j in range(8,19):
-                button_temp.append(self.data_list[j])
+            for j in range(5, 8): #vr, left joy, right joy (channels 5-7)
+                joystick_temp.append(self.channel[j])
 
-            # Map the joystick values from 0 -> 255 (as a string) to  -1.00 -> 1.00
-            for x in joystick_temp:
-                if x is not '':
-                    x = unpack('B', x)
-                    x = float(x[0])
-                    x = (x / 255.0 * 2)-1
-                    self.axes.append(x)
-                else:
-                    self.axes.append(0.00)
-
-            # Map the D-pad values from -1 -> 0 -> 1 (as a string) to  0 -> 1
-            for x in dpad_temp:
-                if x is not '':
-                    x = unpack('B', x)
-                    x = x[0]
-                    x = x - 1
-                    self.axes.append(x)
-                else:
-                    self.axes.append(1)
-
-            # Map the regular button values from 0 -> 1 (as a string) to  0 -> 1
-            for x in button_temp:
-                if x is not '':
-                    x = unpack('B', x)
-                    x = x[0]
-                    self.buttons.append(x)
+            # Map switches A, B, C, D (labelled as per the manual) 
+            length = len(buttons_temp)
+            for x in range (0, length):
+                if buttons_temp[x] > 0x0700: #should be distinction point between two states of the switch
+                    self.buttons.append(1)
                 else:
                     self.buttons.append(0)
+
+            # Map the joystick values from 0 -> 255 (as a string) to  -1.00 -> 1.00
+            length = len(joystick_temp)
+            for x in (0, length):
+
+                # if x is not '':
+                #     x = unpack('B', x)
+                #     x = float(x[0])
+                #     x = (x / 255.0 * 2)-1
+                #     self.axes.append(x)
+                # else:
+                #     self.axes.append(0.00)
 
         except:
             print "Caught Exception"
 
     def length(self):
-        return len(self.data_list)
+        return len(self.channel)
 
 
 def start():
@@ -91,13 +88,15 @@ def start():
 
     while not rospy.is_shutdown():
         data_length = 0
-        received_data= ""
+        received_data = []
         # Loop to check to make sure we have received a string of proper length with the right formatting
-        while received_data[:2] != "s " or received_data[-4:] != ' e\r\n' or data_length != 19:
-            received_data = ser.readline()
-            if received_data[:2] == "s " and received_data[-4:] == ' e\r\n':
+        #changed parameters below for SBUS
+        while received_data[0] != 0x0F or received_data[24] != 0 or num_channel != 8:  
+            for i in range (0,24):
+                received_data[i] = ser.read(1) #reads one byte
+            if received_data[0] == 0x0F and received_data[24] == 0:
                 receiver = parse_receiver(received_data)
-                data_length = receiver.length()
+                num_channel = receiver.length()
         # Once a proper packet has been received, parse it
         receiver.parse()
         # Setup a JOY message with the parsed data and publish it
