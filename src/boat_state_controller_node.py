ba1f9beeffd849c4cd7fd2b22b6e0b85f7b2c307
Matthew Reynolds <rey.mtr@gmail.com>
2018-01-17 20:12:59 -0500

    Clarified the operation of the boat_state_controller node
    This node switches the boat state from RC to AUTO and back.

--- src/boat_state_controller_node.py
+++ src/boat_state_controller_node.py
@@ -4,17 +4,14 @@ from sensor_msgs.msg import Joy
 from std_msgs.msg import Bool
 from boat_nav.msg import BoatState
 
-state = 0
+state = BoatState()
       
 import time
 pub = rospy.Publisher('boat_state', BoatState, queue_size=10)
 
-
 def state_callback(new_state):
     global state
     state = new_state
-
-
 
 def joy_callback(controller):
     global pub
@@ -22,19 +19,25 @@ def joy_callback(controller):
     rate = rospy.Rate(10)
 
     # If R1 is pushed and L1 is not the set autonomous mode
-
-    if controller.buttons[5] == 1 and controller.buttons[4] == 0:
+    if controller.buttons[5] == 1 and controller.buttons[4] == 0 and state.major != MAJ_AUTONOMOUS:
         state.major = BoatState.MAJ_AUTONOMOUS
+        pub.publish(state)
+        
     # If L1 is pushed and R1 is not then set manual mode
-    elif controller.buttons[5] == 0 and controller.buttons[4] == 1:
+    elif controller.buttons[5] == 0 and controller.buttons[4] == 1 and state.major != MAJ_RC:
         state.major = BoatState.MAJ_RC
-
-    pub.publish(state)
+        pub.publish(state)
+        
+    # Add a button to set the state to disabled!
+    elif false:
+        state.major = BoatState.MAJ_DISABLED
+        pub.publish(state)
+    
     rate.sleep()
 
 def listener():
     # Setup subscribers
-    rospy.init_node('rc_auto_node', anonymous=True)
+    rospy.init_node('controller_node', anonymous=True)
     rospy.Subscriber('joy', Joy, joy_callback)
     rospy.Subscriber('boat_state', BoatState, state_callback)
     rospy.spin()
